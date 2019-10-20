1f8b19ef1aa9f36e97a584985ec836562cb2f766
Leigh Forrest <leighmforrest@gmail.com>
2019-10-20 14:04:33 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    fabs = ['john', 'paul', 'george', 'ringo']
+    return jsonify({'fabs': fabs})
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
